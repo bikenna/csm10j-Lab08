5b361ea06cf737b944cd15755721bb21627cd29d
bikenna <brianikenna@gmail.com>
2015-10-16 10:53:51 -0700

    Create MotorVehicle.java

---
+++ src/partC/MotorVehicle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partC;
+
+/**
+ *
+ * @author brianobioha
+ */
+public abstract class MotorVehicle {
+    public MotorVehicle(String name){ this.name = name;}
+    
+    abstract void TurnOn();
+    abstract void TurnOff();
+    
+    private String name;
+    
+    public String getName(){
+        return name;
+    }
+}
